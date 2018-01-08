8fca4248358bed598163a13e662b8524c194f9be
Kevin <kcalvin@outlook.com>
2018-01-08 14:23:35 -0500

    Started with collectionView

---
+++ EMS/Controller/HomeHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  HomeHeaderView.swift
+//  EMS
+//
+//  Created by Kevin C on 1/8/18.
+//  Copyright © 2018 KevinC. All rights reserved.
+//
+
+import UIKit
+
+class HomeHeaderView: UICollectionReusableView {
+    
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var fullName: UILabel!
+    @IBOutlet weak var jobTitle: UILabel!
+        
+}
